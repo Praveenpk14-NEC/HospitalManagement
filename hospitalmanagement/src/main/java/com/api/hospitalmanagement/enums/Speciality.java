fcecaec5984390e7f782b37ac93925544e69c869
Praveen <140406551+Praveenpk14-NEC@users.noreply.github.com>
2025-04-15 17:32:26 +0530

    Delete hospitalmanagement directory

--- hospitalmanagement/src/main/java/com/api/hospitalmanagement/enums/Speciality.java
+++
@@ -1,5 +0,0 @@
-package com.api.hospitalmanagement.enums;
-
-public enum Speciality {
-	PHYSICIAN, ORTHO, GYNAC;
-}
--- hospitalmanagement/src/main/java/com/api/hospitalmanagement/repository/AuthRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.hospitalmanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api.hospitalmanagement.model.User;
-
-public interface AuthRepository extends JpaRepository<User, Integer>{
-	User findByUsername(String username);
-}
